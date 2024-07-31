dcda70d0cf96ef0072daaafa2d30e2f3dc070bc2
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-07-31 15:05:37 -0400

    practiced adding several list items using React

---
+++ index.js
@@ -0,0 +1,7 @@
+// ReactDOM.render(<h1>Hello, world!! I am learning REACT</h1>, document.querySelector('#root'));
+ReactDOM.render(
+
+<ul>
+    <li>List item 1</li>
+    <li>List item 2</li>
+</ul>, document.getElementById('root'))
